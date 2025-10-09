b5fc8f730a5e6523209df244ef969d66103d3b2d
AHNXD <nzamlyalhady@gmail.com>
2025-10-09 14:48:21 +0300

    add assets to assets class

--- lib/core/utils/assets_data.dart
+++ lib/core/utils/assets_data.dart
@@ -1,3 +1,7 @@
 class AssetsData {
-  static const logo = 'assets/images/tabiby.png';
+  static const appIcon = 'assets/icons/appIcon.png';
+  static const logoBlack = 'assets/images/logo_black.png';
+  static const logoWhite = 'assets/images/logo_white.png';
+  static const logoGreen = 'assets/images/logo_green.png';
+  static const logoBlue = 'assets/images/logo_blue.png';
 }
