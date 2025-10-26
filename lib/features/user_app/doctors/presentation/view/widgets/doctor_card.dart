bb93f76d65050d0a71cbf79f8529531d0e27b33c
AHNXD <nzamlyalhady@gmail.com>
2025-10-26 22:20:44 +0300

    fix

--- lib/features/user_app/doctors/presentation/view/widgets/doctor_card.dart
+++ lib/features/user_app/doctors/presentation/view/widgets/doctor_card.dart
@@ -33,7 +33,7 @@ class DoctorCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(18),
           boxShadow: [
             BoxShadow(
-              color: Colors.grey.withOpacity(0.2),
+              color: Colors.grey.withValues(alpha: 0.2),
               blurRadius: 8,
               spreadRadius: 2,
             ),
